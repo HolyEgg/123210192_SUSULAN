0a68865b344e454d74f54b45ecf6462a737a0719
HolyEgg <103435924+HolyEgg@users.noreply.github.com>
2023-06-07 12:22:41 +0700

    Add files via upload

---
+++ Responsi_123210192/src/Koneksi/Responsi_Koneksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Koneksi;
+
+import com.mysql.jdbc.MysqlDataSource;
+
+
+/**
+ *
+ * @author PC PRAKTIKUM
+ */
+public class Responsi_Koneksi {
+    
+    static.Connection con;
+    
+    public
+    
+}
